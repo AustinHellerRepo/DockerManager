924864fc86c1e68b9c267105ab54d762bd973a87
austin <galaky2005@gmail.com>
2021-11-07 15:28:30 -0500

    added method skeleton;

--- src/austin_heller_repo/docker_manager.py
+++ src/austin_heller_repo/docker_manager.py
@@ -3,4 +3,16 @@ from typing import List, Tuple, Dict
 
 
 class DockerManager():
-	pass
+
+	def __init__(self, *, dockerfile_directory_path: str):
+
+		self.__dockerfile_directory_path = dockerfile_directory_path
+
+	def start(self):
+		raise NotImplementedError()
+
+	def execute_command(self, *, command: str):
+		raise NotImplementedError()
+
+	def stop(self):
+		raise NotImplementedError()
