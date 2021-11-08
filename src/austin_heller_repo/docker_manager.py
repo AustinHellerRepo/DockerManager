614a927a26e4ded581491e4a1a5e9024589c7b66
austin <galaky2005@gmail.com>
2021-11-07 20:15:19 -0500

    fixed stdout collecting process between container stdout and execute command stdout; working on copy_file;

--- src/austin_heller_repo/docker_manager.py
+++ src/austin_heller_repo/docker_manager.py
@@ -4,6 +4,9 @@ import docker
 from docker.models.containers import Container
 from docker.client import DockerClient
 import re
+import io
+import tarfile
+import os
 
 
 class DockerContainerInstance():
@@ -15,11 +18,38 @@ class DockerContainerInstance():
 		self.__docker_client = docker_client
 		self.__docker_container = docker_container
 
+		self.__stdout = None
+
 	def get_stdout(self) -> str:
-		return self.__docker_container.logs()
+		logs = self.__docker_container.logs()
+		if logs != b"":
+			if self.__stdout is None:
+				self.__stdout = b""
+			self.__stdout += logs
+		if self.__stdout is None:
+			return None
+		else:
+			line = self.__stdout
+			self.__stdout = None
+			return line
 
 	def execute_command(self, *, command: str):
-		self.__docker_container.exec_run(command)
+		lines = self.__docker_container.exec_run(command, stderr=True, stdout=True)
+		if "exec failed" in str(lines) or "409 Client Error" in str(lines):
+			raise Exception(f"execute_command failed: {lines}")
+		for line in lines:
+			if line != 0:
+				if self.__stdout is None:
+					self.__stdout = b""
+				self.__stdout += line
+
+	def copy_file(self, *, source_file_path: str, destination_file_path: str):
+		stream = io.BytesIO()
+		with tarfile.open(fileobj=stream, mode="w|") as tar, open(source_file_path, "rb") as source_file_handle:
+			tar_info = tar.gettarinfo(fileobj=source_file_handle)
+			tar_info.name = os.path.basename(source_file_path)
+			tar.addfile(tar_info, source_file_handle)
+		self.__docker_container.put_archive(destination_file_path, stream.getvalue())
 
 	def stop(self):
 		if self.__docker_container is None:
@@ -56,7 +86,9 @@ class DockerManager():
 			docker_container = self.__docker_client.containers.run(
 				image=image_name,
 				name=container_name,
-				detach=True
+				detach=True,
+				stdout=True,
+				stderr=True
 			)
 			docker_container_instance = DockerContainerInstance(
 				image_name=image_name,
--- test/docker_manager_test.py
+++ test/docker_manager_test.py
@@ -4,6 +4,7 @@ import tempfile
 import docker.models.images
 import docker.errors
 import time
+from datetime import datetime
 
 
 class DockerManagerTest(unittest.TestCase):
@@ -14,7 +15,8 @@ class DockerManagerTest(unittest.TestCase):
 
 		image_names = [
 			"helloworld",
-			"multiple_stdout"
+			"multiple_stdout",
+			"waits_five_seconds"
 		]
 
 		for image_name in image_names:
@@ -131,9 +133,84 @@ class DockerManagerTest(unittest.TestCase):
 
 		self.assertIsNotNone(docker_container_instance)
 
+		time.sleep(1)
+
 		stdout = docker_container_instance.get_stdout()
 
 		self.assertEqual(b"Hello world!\n", stdout)
+
+		docker_container_instance.stop()
+
+		docker_manager.dispose()
+
+	def test_start_helloworld_docker_image_ls_command_failed(self):
+
+		docker_manager = DockerManager(
+			dockerfile_directory_path="./dockerfiles/helloworld"
+		)
+
+		docker_container_instance = docker_manager.start(
+			image_name="test_helloworld_image",
+			container_name="test_helloworld_container"
+		)
+
+		self.assertIsNotNone(docker_container_instance)
+
+		time.sleep(1)
+
+		stdout = docker_container_instance.get_stdout()
+
+		self.assertEqual(b"Hello world!\n", stdout)
+
+		with self.assertRaises(Exception) as ex:
+			docker_container_instance.execute_command(
+				command="ls"
+			)
+
+		#self.assertIn("execute_command failed: ", str(ex.exception))
+		self.assertRegex(str(ex.exception), "Container [a-f0-9]+ is not running")
+
+		docker_container_instance.stop()
+
+		docker_manager.dispose()
+
+	def test_start_waits_five_seconds_docker_image_ls_command(self):
+
+		docker_manager = DockerManager(
+			dockerfile_directory_path="./dockerfiles/waits_five_seconds"
+		)
+
+		print(f"time 0: {datetime.utcnow()}")
+
+		docker_container_instance = docker_manager.start(
+			image_name="test_waits_five_seconds_image",
+			container_name="test_waits_five_seconds_container"
+		)
+
+		print(f"time 1: {datetime.utcnow()}")
+
+		self.assertIsNotNone(docker_container_instance)
+
+		stdout = docker_container_instance.get_stdout()
+
+		print(f"time 2: {datetime.utcnow()}")
+
+		#self.assertEqual(b"", stdout)
+		self.assertIsNone(stdout)
+
+		docker_container_instance.execute_command(
+			command="ls"
+		)
+
+		print(f"time 3: {datetime.utcnow()}")
+
+		time.sleep(1)
+
+		stdout = docker_container_instance.get_stdout()
+
+		print(f"time 4: {datetime.utcnow()}")
+
+		print(f"stdout: {stdout}")
 
 		docker_container_instance.stop()
 
@@ -157,6 +234,41 @@ class DockerManagerTest(unittest.TestCase):
 		stdout = docker_container_instance.get_stdout()
 
 		self.assertEqual(b"first\nsecond\n", stdout)
+
+		docker_container_instance.stop()
+
+		docker_manager.dispose()
+
+	def _test_copying_file_to_container(self):
+
+		temp_file = tempfile.TemporaryFile()
+
+		temp_file.write("test")
+		temp_file.flush()
+
+		docker_manager = DockerManager(
+			dockerfile_directory_path="./dockerfiles/helloworld"
+		)
+
+		docker_container_instance = docker_manager.start(
+			image_name="test_helloworld_image",
+			container_name="test_helloworld_container"
+		)
+
+		self.assertIsNotNone(docker_container_instance)
+
+		stdout = docker_container_instance.get_stdout()
+
+		self.assertEqual(b"Hello world!\n", stdout)
+
+		docker_container_instance.copy_file(
+			source_file_path=temp_file.name,
+			destination_file_path="test.txt"
+		)
+
+		docker_container_instance.execute_command(
+			command="ls"
+		)
 
 		docker_container_instance.stop()
 
